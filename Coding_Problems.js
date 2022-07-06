057d329f2a1214d1b8069d00403cf903949cdb52
Jacob Lee <jacobhylee@gmail.com>
2022-07-07 07:37:56 +1000

    Co-authored-by: Ju An Kang <doubtful.mr@gmail.com>

--- Coding_Problems.js
+++ Coding_Problems.js
@@ -2,17 +2,42 @@
 
 const test = [1, 2, 3, 4, 5];
 
+// but this works too
+// you can also use array.unshift() and just iterate from front to back
+// since you'll be adding a value to the head of the array each time
+// by the end of loop it will return you a reversed array
+
 function reversearray(input) {
-  let ret = new Array();
-  for (let i = input.length - 1; i > -1; i--) {
-    ret.push(input[i]);
+  const ret = []; // This is the best conventional way for 2 reasons:
+  for (let i = input.length - 1; i >= 0; i--) {
+    input.splice(input[i]);
   }
 
-  return ret;
+  return input;
 }
 
 reversearray(test);
 console.log(reversearray(test));
+
+const reverseSameArray = (arr) => {
+  // Go through only half of the array, and swap current index with the mirroring index from the end
+  for (let i = 0; i < arr.length / 2; i++) {
+    // [a,b] = [b,a] lets you swap in js.
+    // there's a more standard way using a temp variable, but this is simpler
+    [arr[i], arr[arr.length - 1 - i]] = [arr[arr.length - 1 - i], arr[i]];
+
+    /// long way:
+    const temp = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = temp;
+  }
+};
+const SampleArr = [1, 2, 3, 4];
+console.log(SampleArr); // [1,2,3,4];
+reverseSameArray(SampleArr);
+console.log(SampleArr); // [4,3,2,1];
+reverseSameArray(SampleArr);
+console.log(SampleArr); // [1,2,3,4];
 
 //print all even numbers from 1 - 10
 //did it first try lessgooo
