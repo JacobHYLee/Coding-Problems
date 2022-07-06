dc9bb5b97c1cb6345bf27def0ce9f8e8dca440f3
Jacob Lee <jacobhylee@gmail.com>
2022-07-07 06:16:07 +1000

    More questions!

--- Coding_Problems.js
+++ Coding_Problems.js
@@ -1,4 +1,4 @@
-//Reverse an array
+//function to reverse an array
 
 const test = [1, 2, 3, 4, 5];
 
@@ -13,3 +13,37 @@ function reversearray(input) {
 
 reversearray(test);
 console.log(reversearray(test));
+
+//print all even numbers from 1 - 10
+//did it first try lessgooo
+
+for (let i = 1; i < 11; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+//create a length conversion function, km to miles
+//did it first try lessgooo
+function convert(km) {
+  let miles = km * 0.621;
+  return `${km} kilometers is equal to ${miles} miles!`;
+}
+
+console.log(convert(15));
+
+//calculate the sum of numbers within an array
+
+const example = [1, 2, 3, 4, 5, 6, 7, -1, -3, -5];
+
+function sumOfArrays(input) {
+  let sum = 0;
+  for (let i = 0; i < input.length; i++) {
+    sum += input[i];
+  }
+  return sum;
+}
+
+console.log(sumOfArrays(example));
+
+//sort an array from lowest to highest
