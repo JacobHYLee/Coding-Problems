2ecb9f3aef0e329628fb42b3bd4109338e302553
Jacob Lee <jacobhylee@gmail.com>
2022-07-12 23:50:05 +1000

    counting vowels

--- Coding_Problems.js
+++ Coding_Problems.js
@@ -80,3 +80,42 @@ function sortArray(input) {
 }
 
 console.log(sortArray(testArray));
+
+//eliminate negative numbers in an array
+
+const negativeArray = [-1, 3, 2, 5, -5, -3, 4, -2, 9];
+
+function filterArray(input) {
+  const filteredArray = [];
+  for (let i = 0; i < input.length; i++)
+    if (input[i] >= 0) {
+      filteredArray.push(input[i]);
+    }
+  return filteredArray;
+}
+
+console.log(filterArray(negativeArray));
+
+//count the number of vowels in a string
+
+const str = "I really need a dev job quick!";
+
+function countVowels(str) {
+  const vowels = [];
+  const lowerCase = str.toLowerCase();
+  for (let i = 0; i < lowerCase.length; i++) {
+    if (
+      lowerCase[i] === "a" ||
+      lowerCase[i] === "e" ||
+      lowerCase[i] === "i" ||
+      lowerCase[i] === "o" ||
+      lowerCase[i] === "u"
+    ) {
+      vowels.push(lowerCase[i]);
+    }
+  }
+
+  return vowels.length;
+}
+
+console.log(countVowels(str));
